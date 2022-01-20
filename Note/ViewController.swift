df1460154317f33f74b100147f01a996ed76c0a2
Denny McYuri <nordicwolf.d@gmail.com>
2022-01-20 19:49:59 +0200

    Initial Commit

---
+++ Note/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Note
+//
+//  Created by Denny on 20.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
